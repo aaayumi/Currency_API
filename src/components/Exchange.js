0ced9e1e99cecca5d07736086c7837bbed092f5b
Ayumi Saito <ayumisaito27@gmail.com>
2017-10-25 18:34:00 +0200

    add map function

--- src/components/Exchange.js
+++ src/components/Exchange.js
@@ -7,78 +7,100 @@ const date = '2017-10-22';
 const currencies = 'USD,EUR,CAD,AUD';
 const format = 1;
 
-
 export default class Exchange extends React.Component {
 
-    constructor(props) {
-    	super(props);
-    	this.state = {
-    		item : [],
-    		newItem : []
-    	};
-    	this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: [],
+      newItem: []
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-	componentDidMount(){
-	const url = `${baseUrl}${API_KEY}&date=${date}&currencies=${currencies}&format=${format}`
-	console.log(url)
+  componentDidMount() {
+    const url = `${baseUrl}${API_KEY}&date=${date}&currencies=${currencies}&format=${format}`
     axios.get(url)
          .then(({data}) => {
-         	console.log(data);
-         	this.setState({
-         		item: data.quotes
-         	});
-         	 console.log(this.state.item.USDAUD)
+         	console.log(data.quotes);
+         	this.state.item.push(data.quotes);
+         	 this.setState({
+            item: this.state.item
+             });
          })
-         .catch((err) => {})
-	 }
+         .catch((err) => {
+         	console.log(err);
+         })
 
-	 handleSubmit() {
-	 	//get date value
-	 	var value = new Date( document.getElementById('dateInput').value);
-	 	console.log(value)
-        // date format YYYY-MM-DD
-        var day = value.getDate();
-	    var month = value.getMonth() + 1;
-	    var year = value.getFullYear();
+  }
 
-	    var thisDate = [year, month, day].join('-');
-	    console.log(thisDate)
-	 
-	    const newUrl = `${baseUrl}${API_KEY}&date=${thisDate}&currencies=${currencies}&format=${format}`
+  handleSubmit() {
+   // clear the previous data
+   this.setState({ 
+  		newItem: []
+  	});
+
+    //get date value
+    var value = new Date( document.getElementById('dateInput').value);
+
+   // date format YYYY-MM-DD
+    var day = value.getDate();
+    var month = value.getMonth() + 1;
+    var year = value.getFullYear();
+
+    var thisDate = [year, month, day].join('-');
+    console.log(thisDate)
         
-          axios.get(newUrl)
-         .then(({data}) => {
+    const newUrl = `${baseUrl}${API_KEY}&date=${thisDate}&currencies=${currencies}&format=${format}`
+
+     axios.get(newUrl)
+          .then(({data}) => {
          	console.log(data);
+         	this.state.newItem.push(data.quotes);
          	this.setState({
-         		newItem: data.quotes
-         	});
-         	 console.log(this.state.newItem)
+            newItem: this.state.newItem
+             });
+         	console.log(this.state.newItem)
          })
-         .catch((err) => {})
+         .catch((err) => {
+         	console.log(err);
+         })
 
-	 }
+}
 
-	render() {
-	/*console.log("res" + this.state.item.USDAUD)
-	const rate = this.state.item.map((element, index)=>{
-		return <div key={index}>
-		       <p>{element}</p>
-		       </div>
-	});
-	console.log("rate" + rate)*/
-		return (
-			<div>
-			<div>Exchange</div>
-			<ul>Today's rate
-			<li>{this.state.item.USDAUD}</li>
-			</ul>
 
-			<input type="date" onChange={this.handleSubmit} id="dateInput" />
-			<ul>rate
-			<li>{this.state.newItem.USDAUD}</li>
-			</ul>
-           </div>
-			)
-	}
+  render() {
+    const rate = this.state.item.map((quotes, index) => {
+      return Object.keys(quotes).map(key => {
+        return (
+            <div key = {key} >
+              <p>{quotes[key]}</p>
+            </div>
+          )
+      })
+    });
+
+    const newRate = this.state.newItem.map((quotes, index) => {
+    	return Object.keys(quotes).map(key => {
+    		return (
+    			<div key = {key} >
+    			<p>{quotes[key]}</p>
+    			</div>
+    			)
+    	})
+    })
+
+    console.log({rate} - {newRate})
+    return ( 
+      <div>
+        <div>Exchange</div> 
+          Today 's rate
+          <p>{rate}</p> 
+          <p> {newRate} </p>
+         
+        <input type="date" onChange={this.handleSubmit} id="dateInput" />
+    
+     </div>
+    )
+  }
 }
